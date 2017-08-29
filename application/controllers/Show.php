f145677cfacbd1328c6a9946d15121de7a16f9ef
yulin <506837406@qq.com>
2017-08-29 15:58:15 +0800

    create new BaseClass and Show

---
+++ application/controllers/Show.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Show extends Base {
+
+	public function index(){
+		echo 'this is showClass extends BaseClass';
+	}
+
+	public function addView($view ='home' ){
+
+	}
+
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,7 @@ class Welcome extends CI_Controller {
 	}
 
 	/**
-	 * ciœ‘ æ
+	 * ciÂºÄÂßã
 	 */
 	public function show(){
 		var_dump($_SERVER);
