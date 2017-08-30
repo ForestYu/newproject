be622cb7048f9693daff4ebdbb72c89dbb79b5db
yulin <506837406@qq.com>
2017-08-30 11:44:28 +0800

    api相关添加

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Base extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+	}
+
 	public function index()
 	{
 		echo 'this is baseIndex';
